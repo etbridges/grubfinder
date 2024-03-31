350be5d16992fea983af7288dc5d179d854f614b
etbridges <133754189+etbridges@users.noreply.github.com>
2024-03-31 19:01:38 +0200

    updated the 404 page style

--- client/src/routes/+error.svelte
+++ client/src/routes/+error.svelte
@@ -2,6 +2,17 @@
 </script> 
 
 <div> 
-    <p>Looks like you lost your way</p> 
+    <h2 class="content-title">Looks Like You Lost Your Way</h2>
     <a class="btn btn-primary" href="/">Go to home page</a> 
 </div>
+
+
+<style>
+    .content-title {
+        font-family: 'Baloo 2 Variable', sans-serif;
+        color: #890000;
+        font-weight: 700;
+        margin-top: 12px;
+    }
+
+</style>
