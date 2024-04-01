dcc381f1978273618264876d2e69c1dec585e94e
etbridges <133754189+etbridges@users.noreply.github.com>
2024-04-01 16:38:28 +0200

    Finalized 404 page adding feature to send back to previous page or home page

--- client/src/routes/+error.svelte
+++ client/src/routes/+error.svelte
@@ -1,9 +1,20 @@
-<script> import { page } from '$app/stores'; 
+<script> 
+import { page } from '$app/stores';
+function goBack() {
+    if (document.referrer) {
+      // If there's a referrer, go back to the previous page
+      window.history.back();
+    } else {
+      // If there's no referrer, navigate to the homepage
+      window.location.href = "/";
+    }
+  };
 </script> 
 
 <div> 
-    <h2 class="content-title">Looks Like You Lost Your Way</h2>
-    <a class="btn btn-primary" href="/">Go to home page</a> 
+    <h2 class="content-title">404 Page Not Found</h2>
+    <p>Looks Like You Lost Your Way</p>
+    <button type="button" class="pill-button" on:click={goBack}>Go Back</button>
 </div>
 
 
@@ -15,4 +26,15 @@
         margin-top: 12px;
     }
 
-</style>
+    .pill-button {
+      display: inline-block;
+      background-color: #890000;
+      color: white;
+      padding: 10px 20px;
+      border-radius: 50px; 
+      text-decoration: none;
+      font-family: 'Baloo 2 Variable', sans-serif;
+      font-size: 16px;
+      cursor: pointer;
+    }
+  </style>
